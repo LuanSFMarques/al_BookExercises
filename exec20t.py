01cf9475fd3a9276d19d7edf36fc7c42bf0feca4
LuanSFMarques <luansfmarques@gmail.com>
2023-05-06 09:18:49 -0300

    Exercise 20 FINISHED

--- exec20t.py
+++ exec20t.py
@@ -1,5 +1,5 @@
 #It takes about 365.25 days for the earth to revolve around the sun. This slight offset would cause
-our 365-day calendar to become inaccurate over time. Therefore, leap years have an extra day,
+#our 365-day calendar to become inaccurate over time. Therefore, leap years have an extra day,
 #February 29th. A leap year occurs on all years divisible by four (e.g., 2016, 2020, 2024, and so on).
 #However, the exception to this rule is that years divisible by one hundred (e.g., 2100, 2200, 2300, and
 #so on) aren’t leap years. And the exception to this exception is that years divisible by four hundred
