2cd5fd6b5cc657c329047844091d531bbcfb4e6c
LuanSFMarques <luansfmarques@gmail.com>
2023-05-10 09:59:49 -0300

    Exercise 27 FINISHE

---
+++ exec27t.py
@@ -0,0 +1,14 @@
+#Write a drawRectangle() function with two integer parameters: width and height. The
+#function doesn’t return any values but rather prints a rectangle with the given number of hashtags in
+#the horizontal and vertical directions.
+
+#finished
+def drawRectangle(width, height):
+    x = 0
+    if width < 1 or height < 1:
+        return print("invalid value")
+    while x < height:
+        print("#"*width)
+        x += 1
+
+drawRectangle(10, 4)
